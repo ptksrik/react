2195a8a7a38ef2d56d5ddb123d187b79dee49907
Riku Pitkanen <n0piri00@students.oamk.fi>
2021-01-24 20:48:55 +0200

    alcometer layout tweaking

--- src/App.js
+++ src/App.js
@@ -22,6 +22,10 @@ function App() {
     else {
       alcoholLevel = gramsRemaining / (weight * 0.6);
     }
+
+    if(alcoholLevel < 0){
+      alcoholLevel = 0;
+    }
     setResult(alcoholLevel);
   }
 
@@ -30,38 +34,44 @@ function App() {
       <h2>Calculating blood alcohol level</h2>
       <form onSubmit={handleSubmit} className="container-fluid">
 
-        <div className="form-group row">
+        <div className="form-group row m-2 p-2">
 
-          <div>
-            <div className="col-sm-2 col-xs-12 p-3">Weight</div>
-            <input className="col-3 p-3" name="weight" type="number" step="1" value={weight} onChange={e => setWeight(e.target.value)}></input>
+          <div className="row my-2">
+            <div className="col-md-2 col-sm-12">Weight</div>
+            <input className="col-3 p-2 bg-light" name="weight" type="number" step="1" value={weight} onChange={e => setWeight(e.target.value)}></input>
           </div>
-          <div>
-            <div className="col-sm-2 col-xs-12 p-3">Bottles</div>
-            <input className="col-3 p-3" name="bottles" type="number" value={bottles} onChange={e => setBottles(e.target.value)}>
+          <div className="row my-2">
+            <div className="col-md-2 col-sm-12">Bottles</div>
+            <input className="col-3 p-2 bg-light" name="bottles" type="number" value={bottles} onChange={e => setBottles(e.target.value)}>
             </input>
           </div>
-          <div>
-            <div className="col-sm-2 col-xs-12 p-3">Time</div>
-            <input className="col-3 p-3" name="time" type="number" value={time} onChange={e => setTime(e.target.value)}>
+          <div className="row my-2">
+            <div className="col-md-2 col-sm-12">Time</div>
+            <input className="col-3 p-2 bg-light" name="time" type="number" value={time} onChange={e => setTime(e.target.value)}>
             </input>
           </div>
-          <div>
-            <div className="col-sm-2 col-xs-12 p-3">Gender</div>
-            <input type="radio" name="gender" value="male" id="male" defaultChecked onChange={e => setGender(e.target.value)} />
-            <label className="col-sm-2 col-xs-12 p-3" for="male">Male</label>
-            <input type="radio" name="gender" value="female" id="female" onChange={e => setGender(e.target.value)} />
-            <label className="col-sm-2 col-xs-12 p-3" for="female">Female</label>
+          <div className="row my-2">
+            <div className="col-md-2 col-sm-12">Gender</div>
+            <div className="col-sm-3 col-xs-12">
+              <div>
+                <input type="radio" name="gender" value="male" id="male" defaultChecked onChange={e => setGender(e.target.value)} />
+                <label for="male" className="mx-2">Male</label>
+                <div>
+                  <input type="radio" name="gender" value="female" id="female" onChange={e => setGender(e.target.value)} />
+                  <label for="female" className="mx-2">Female</label>
+                </div>
+              </div>
+            </div>
           </div>
-          <div>
-            <output className="bg-danger col-3 text-center my-3">{result.toFixed(2)}</output>
+          
+          <div className="row">
+            <output className="bg-light col-3 text-center my-3 p-2">{result.toFixed(2)}</output>
           </div>
-          <div>
-            <button className="btn btn-primary col-3">Calculate</button>
+          <div className="row">
+            <button className="btn btn-primary col-2">Calculate</button>
           </div>
         </div>
       </form>
-
     </>
   );
 }
