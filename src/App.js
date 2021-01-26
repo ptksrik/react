e16e627b020724a0dd213ae78a114c1147380576
Riku Pitkanen <n0piri00@students.oamk.fi>
2021-01-26 13:44:50 +0200

    Alkometri v1.0

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
       alcoholLevel = gramsRemaining / (weight * 0.6);
     }
 
-    if(alcoholLevel < 0){
+    if (alcoholLevel < 0) {
       alcoholLevel = 0;
     }
     setResult(alcoholLevel);
@@ -31,47 +31,48 @@ function App() {
 
   return (
     <>
-      <h2>Calculating blood alcohol level</h2>
-      <form onSubmit={handleSubmit} className="container-fluid">
-
-        <div className="form-group row m-2 p-2">
-
-          <div className="row my-2">
-            <div className="col-md-2 col-sm-12">Weight</div>
-            <input className="col-3 p-2 bg-light" name="weight" type="number" step="1" value={weight} onChange={e => setWeight(e.target.value)}></input>
-          </div>
-          <div className="row my-2">
-            <div className="col-md-2 col-sm-12">Bottles</div>
-            <input className="col-3 p-2 bg-light" name="bottles" type="number" value={bottles} onChange={e => setBottles(e.target.value)}>
-            </input>
-          </div>
-          <div className="row my-2">
-            <div className="col-md-2 col-sm-12">Time</div>
-            <input className="col-3 p-2 bg-light" name="time" type="number" value={time} onChange={e => setTime(e.target.value)}>
-            </input>
-          </div>
-          <div className="row my-2">
-            <div className="col-md-2 col-sm-12">Gender</div>
-            <div className="col-sm-3 col-xs-12">
-              <div>
-                <input type="radio" name="gender" value="male" id="male" defaultChecked onChange={e => setGender(e.target.value)} />
-                <label for="male" className="mx-2">Male</label>
+      <div className="container-fluid">
+        <div className="row m-1 p-1">
+          <header className="ownHeader col-md-6 col-xd-12 text-center">
+            <h2 className="py-2">Calculating blood alcohol level</h2>
+          </header>
+          <form onSubmit={handleSubmit}>
+            <div className="form-group row my-2">
+              <div className="col-md-2 col-sm-12">Weight</div>
+              <input className="col-md-1 p-2 bg-light" name="weight" type="number" min="0" step="1" value={weight} onChange={e => setWeight(e.target.value)}></input>
+            </div>
+            <div className="row my-2">
+              <div className="col-md-2 col-sm-12">Bottles</div>
+              <input className="col-md-1 p-2 bg-light" name="bottles" type="number" min="0" value={bottles} onChange={e => setBottles(e.target.value)}>
+              </input>
+            </div>
+            <div className="row my-2">
+              <div className="col-md-2 col-sm-12">Time</div>
+              <input className="col-md-1 p-2 bg-light" name="time" type="number" min="0" value={time} onChange={e => setTime(e.target.value)}>
+              </input>
+            </div>
+            <div className="row my-2">
+              <div className="col-md-2 col-sm-12">Gender</div>
+              <div className="col-md-2 col-xs-12">
                 <div>
-                  <input type="radio" name="gender" value="female" id="female" onChange={e => setGender(e.target.value)} />
-                  <label for="female" className="mx-2">Female</label>
+                  <input type="radio" name="gender" value="male" id="male" defaultChecked onChange={e => setGender(e.target.value)} />
+                  <label for="male" className="mx-2">Male</label>
+                  <div>
+                    <input type="radio" name="gender" value="female" id="female" onChange={e => setGender(e.target.value)} />
+                    <label for="female" className="mx-2">Female</label>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          
-          <div className="row">
-            <output className="bg-light col-3 text-center my-3 p-2">{result.toFixed(2)}</output>
-          </div>
-          <div className="row">
-            <button className="btn btn-primary col-2">Calculate</button>
-          </div>
+            <div className="col-md-6 col-xd-12 text-center result">
+              <output className="text-center my-1 p-2">{result.toFixed(2)}</output>
+            </div>
+            <div className="col-md-6 col-xd-12 text-center mt-3">
+              <button className="btn btn-danger">Calculate</button>
+            </div>
+          </form>
         </div>
-      </form>
+      </div>
     </>
   );
 }
